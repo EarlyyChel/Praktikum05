03058d34a2d10eaa563fd83e561002086f371cc0
EarlyyChel <earlyyychelsealia@gmail.com>
2024-10-02 21:38:54 +0700

    hari

--- PemilihanBilangan10.java
+++ PemilihanBilangan10.java
@@ -1,6 +1,8 @@
+
 import java.util.Scanner;
 
 public class PemilihanBilangan10 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan sebuah angka: ");
